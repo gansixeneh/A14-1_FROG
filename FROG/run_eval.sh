5f445cff396f11e77bc9f799240d77b04053dd3f
Muflih Naufal Maxi <muflihmaxi@gmail.com>
2025-02-17 16:29:56 +0700

    GAS

--- FROG/run_eval.sh
+++ FROG/run_eval.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# models=(
-#     "nvidia/Mistral-NeMo-12B-Instruct"
-#     "meta-llama/Llama-3.1-8B-Instruct"
-#     "Qwen/Qwen2.5-Coder-7B-Instruct"
-#     "Qwen/Qwen2.5-7B-Instruct"
-# )
+models=(
+    "nvidia/Mistral-NeMo-12B-Instruct"
+    "meta-llama/Llama-3.1-8B-Instruct"
+    "Qwen/Qwen2.5-Coder-7B-Instruct"
+    "Qwen/Qwen2.5-7B-Instruct"
+)
 
 # models=(
 #     "Qwen/Qwen2.5-3B-Instruct"
@@ -15,9 +15,9 @@
 #     "mistralai/Mistral-Nemo-Instruct-2407"
 # )
 
-models=(
-    "mistralai/Mistral-Nemo-Instruct-2407"
-)
+# models=(
+#     "mistralai/Mistral-Nemo-Instruct-2407"
+# )
 
 
 for model in "${models[@]}"; do
@@ -28,7 +28,7 @@ for model in "${models[@]}"; do
         --knowledge-source="legal" \
         --model-name="$model" \
         --log-file-path="logs/log_$model.txt" \
-        --test-df-path="data/eval/legal_test_sedikit_banget.json" \
+        --test-df-path="data/eval/legal_test.json" \
         --llm-try-threshold=5 \
         --local-model \
         --no-local-weaviate-client \
