f736e419c341b1c2d0206c4e860c90efbb8f815e
Muflih Naufal Maxi <muflihmaxi@gmail.com>
2025-03-04 12:15:52 +0700

    add more modifications

--- FROG/data/legal_turtle/modify_legal_kg.py
+++ FROG/data/legal_turtle/modify_legal_kg.py
@@ -1,19 +1,27 @@
-from rdflib import Graph, Literal
+from rdflib import Graph, Literal, URIRef
 
 
 graph = Graph()
 graph.parse("data-lex2kg.ttl")
 
-property_uri = "https://example.org/lex2kg/ontology/disahkanDi"
-
-incorrect_literals = {"Jakart", "Jakar", "Jakarta,", "D", "Djakarta"}
 
 for subj, pred, obj in graph:
-    if pred.endswith(property_uri) and isinstance(obj, Literal):
+    # Fix inconsistent city names on lex2kg-o:disahkanDi
+    if pred.endswith("disahkanDi") and isinstance(obj, Literal):
+        incorrect_literals = {"Jakart", "Jakar", "Jakarta,", "D", "Djakarta"}
         if obj.value in incorrect_literals:
             graph.remove((subj, pred, obj))
             graph.add((subj, pred, Literal("Jakarta")))
 
+    # Remove unnecessary /text suffix on lex2kg-o:merujuk
+    if isinstance(subj, URIRef) and subj.endswith("/text"):
+        new_subj = URIRef(str(subj).replace("/text", ""))
+        graph.remove((subj, pred, obj))
+        graph.add((new_subj, pred, obj))
+
+    # Remove all triples with lex2kg-o:bagianDari
+    if pred.endswith("bagianDari"):
+        graph.remove((subj, pred, obj))
 
 graph.serialize(destination="modified_data-lex2kg.ttl", format="turtle")
 
